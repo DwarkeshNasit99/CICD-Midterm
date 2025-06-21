01fdb6f91cbd26940fa0b1bab94c0e5581723aab
DwarkeshNasit99 <Dnasit5836@conestogac.on.ca>
2025-06-21 16:04:09 -0400

    feat: implement and export all utility functions

---
+++ src/stringUtils.js
@@ -0,0 +1,24 @@
+const reverse = (str) => str.split('').reverse().join('');
+
+const capitalize = (str) => {
+  if (!str) return '';
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+const countVowels = (str) => {
+  if (!str) return 0;
+  const vowels = 'aeiouAEIOU';
+  let count = 0;
+  for (const char of str) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+};
+
+module.exports = {
+  reverse,
+  capitalize,
+  countVowels,
+};
