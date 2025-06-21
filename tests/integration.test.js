241733e1391e21373392a7755d6a3d281795090f
DwarkeshNasit99 <Dnasit5836@conestogac.on.ca>
2025-06-21 16:36:05 -0400

    fix(tests): correct import in integration tests

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../src/app');
+const { app } = require('../src/app');
 
 describe('API Integration Tests', () => {
   describe('GET /api/welcome', () => {
